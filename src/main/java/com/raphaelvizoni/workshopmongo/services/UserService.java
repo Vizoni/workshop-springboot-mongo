39d570ba0e30fb21a37b58b8160ffdfbaaa5d770
Raphael Vizoni do Prado <ravizoni@gmail.com>
2026-09-23 14:43:11 -0300

    Insercao de usuario com POST

--- src/main/java/com/raphaelvizoni/workshopmongo/services/UserService.java
+++ src/main/java/com/raphaelvizoni/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.raphaelvizoni.workshopmongo.services;
 
 import com.raphaelvizoni.workshopmongo.domain.User;
+import com.raphaelvizoni.workshopmongo.dto.UserDTO;
 import com.raphaelvizoni.workshopmongo.repository.UserRepository;
 import com.raphaelvizoni.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO) {
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
